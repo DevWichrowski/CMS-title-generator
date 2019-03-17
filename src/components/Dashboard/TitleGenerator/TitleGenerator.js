76bd5b59eca39d4c799cd646079013e543ffd845
devWichrowski <wichrowski.patryk92@gmail.com>
2019-03-17 20:22:05 +0100

    Add <h1> and hr to title generator

--- src/components/Dashboard/TitleGenerator/TitleGenerator.js
+++ src/components/Dashboard/TitleGenerator/TitleGenerator.js
@@ -1,19 +1,21 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { getLoginStatus } from '../../../store/selectors/users.selectors';
+import './TitleGenerator.scss';
 
 class TitleGenerator extends Component {
-  render() {
-    return (
-      <div>
-        <h1>generator</h1>
-      </div>
-    )
-  }
+	render() {
+		return (
+			<div className="title-generator">
+				<h1>Generator</h1>
+        <hr />
+			</div>
+		);
+	}
 }
 
 const mapStateToProps = (state) => ({
 	loggedIn: getLoginStatus(state)
 });
 
-export default connect(mapStateToProps, null)(TitleGenerator)
+export default connect(mapStateToProps, null)(TitleGenerator);
