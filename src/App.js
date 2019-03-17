6fd32c45fc4398527354d2af97beca170ccb86a0
devWichrowski <wichrowski.patryk92@gmail.com>
2019-03-17 19:21:38 +0100

    Add Login component to routing

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { BrowserRouter, Route } from 'react-router-dom';
 import Menu from './components/Menu/Menu';
 import Dashboard from './components/Dashboard/Dashboard';
 import Signup from './components/Signup/Signup';
+import LoginForm from './components/LoginForm/LoginForm';
 
 class App extends Component {
 	render() {
@@ -13,6 +14,7 @@ class App extends Component {
 					<Menu />
 					<Route exact path="/" component={Dashboard} />
 					<Route path="/signup" component={Signup} />
+					<Route path="/login" component={LoginForm} />
 				</div>
 			</BrowserRouter>
 		);
--- src/components/LoginForm/LoginForm.js
+++ src/components/LoginForm/LoginForm.js
@@ -6,25 +6,21 @@ export default class LoginForm extends Component {
 	constructor(props) {
 		super(props);
 		this.state = {};
-    }
-    
-    
+	}
+
 	render() {
 		return (
 			<div className="login-form">
+				<h1>Login</h1>
 				<Form>
 					<Form.Group controlId="formBasicEmail">
 						<Form.Label>Email address</Form.Label>
 						<Form.Control type="email" placeholder="Enter email" />
 						<Form.Text className="text-muted">We'll never share your email with anyone else.</Form.Text>
 					</Form.Group>
-
 					<Form.Group controlId="formBasicPassword">
 						<Form.Label>Password</Form.Label>
 						<Form.Control type="password" placeholder="Password" />
-					</Form.Group>
-					<Form.Group controlId="formBasicChecbox">
-						<Form.Check type="checkbox" label="Check me out" />
 					</Form.Group>
 					<Button variant="primary" type="submit">
 						Submit
