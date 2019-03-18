0fb3a009ed441510f1dfd4d9c240f53200ac243f
devWichrowski <wichrowski.patryk92@gmail.com>
2019-03-18 22:02:26 +0100

    Add saveTitles and saveUrls to save to state.

--- src/components/Dashboard/TitleGenerator/TitleGenerator.js
+++ src/components/Dashboard/TitleGenerator/TitleGenerator.js
@@ -5,6 +5,30 @@ import './TitleGenerator.scss';
 import { Button } from 'react-bootstrap';
 
 class TitleGenerator extends Component {
+	constructor(props) {
+		super(props);
+
+		this.state = {
+			titlesArray: [],
+			urlsArray: [],
+			separator: '\n'
+		};
+	}
+
+	parseStringToArray = (urlString) => {
+		return urlString.split(this.state.separator);
+	};
+
+	saveTitle = (event) => {
+		this.setState({ titlesArray: this.parseStringToArray(event.target.value) });
+		console.log('titleArray', this.state.titlesArray);
+	};
+
+	saveUrls = (event) => {
+		this.setState({ urlsArray: this.parseStringToArray(event.target.value) });
+		console.log('urlsArray', this.state.urlsArray);
+	};
+
 	render() {
 		return (
 			<div className="title-generator">
@@ -12,11 +36,11 @@ class TitleGenerator extends Component {
 				<hr />
 				<div className="titles-textarea">
 					<p>Enter all titles below</p>
-					<textarea className="custom-textarea" />
+					<textarea className="custom-textarea" onChange={this.saveTitle} />
 				</div>
 				<div className="ulrs-textarea">
 					<p>Enter all urls below</p>
-					<textarea className="custom-textarea" />
+					<textarea className="custom-textarea" onChange={this.saveUrls} />
 				</div>
 				<Button className="generate-button" variant="danger">
 					Generate and copy to clipboard
