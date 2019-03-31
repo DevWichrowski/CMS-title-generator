447e6b277455f6bf2a779320e7cefbbba5b73ec5
Patryk Wichrowski <devwichrowski-macbookpro@MacBook-Pro-Patryk.local>
2019-03-31 21:21:11 +0200

    Updates

---
+++ precache-manifest.9debfd8269356c713b0a45d37569984e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "24c67dc69d24f3a90eef",
+    "url": "/CMS-title-generator/static/css/main.056f60a6.chunk.css"
+  },
+  {
+    "revision": "24c67dc69d24f3a90eef",
+    "url": "/CMS-title-generator/static/js/main.bf1748f1.chunk.js"
+  },
+  {
+    "revision": "96fba0eac94d54079570",
+    "url": "/CMS-title-generator/static/js/runtime~main.c07be18a.js"
+  },
+  {
+    "revision": "ae7c4c3d39eda4bdbce0",
+    "url": "/CMS-title-generator/static/css/2.7369f639.chunk.css"
+  },
+  {
+    "revision": "ae7c4c3d39eda4bdbce0",
+    "url": "/CMS-title-generator/static/js/2.e09d937f.chunk.js"
+  },
+  {
+    "revision": "69371ee10f2e3eebcc8c1b976359671b",
+    "url": "/CMS-title-generator/static/media/me.69371ee1.png"
+  },
+  {
+    "revision": "9a50a6a138a39ae04442ea6d037f6376",
+    "url": "/CMS-title-generator/index.html"
+  }
+];
